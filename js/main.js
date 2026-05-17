1908282593f604c078e42032f13e43b66851c166
Raphael Muniz <raphaelmunizvarela@gmail.com>
2026-05-17 16:03:04 -0300

    ok

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 import ApiService from "./api.js";
 import CalculatorUI from "./ui.js";
 
+const isLocalhost = false;
 
 document.addEventListener("DOMContentLoaded", () => {
-	const baseUrl = "http://localhost:8080/capitalizacao-simples";
+	const baseUrl = isLocalhost ? 
+	"http://localhost:8080/capitalizacao-simples" : 
+	"https://spring-calculadora-financeira.onrender.com/capitalizacao-simples";
 
 	const apiService = new ApiService(baseUrl);
 	const app = new CalculatorUI(apiService);
